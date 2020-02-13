d05b3f6202a31a33be2f2cccae5e894fc254394a
satriahabib <satriatife@gmail.com>
2020-02-13 08:09:43 +0700

    initial commit

---
+++ src/main/java/com/example/demo/usercontroller/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.usercontroller;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	List<User> findAll();
+}
